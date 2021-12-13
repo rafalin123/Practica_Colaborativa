01c5fd6de335a3996249b8ebe1cea53512373fba
Medinagrc <5alexmega@gmail.com>
2021-12-13 17:29:01 +0100

    Ambos programas comentados

--- Programa_2.java
+++ Programa_2.java
@@ -11,11 +11,11 @@ public class Programa_2 {
 		System.out.println("Introduzca el porcentaje que desea que quede del isótopo de plutonio");
 		porcentaje = in.nextFloat();
 		do {
-			while ((System.currentTimeMillis()-tiempo)<(26*60*1000));
-			plutonio /= 2f;
+			while ((System.currentTimeMillis()-tiempo)<(26*60*1000)); //Se espera 26 minutos en caso de que de una vuelta entera y en caso contrario se espera los segundos necesarios hasta que quede el porcentaje deseado
+			plutonio /= 2f;  										  //Una vez que pasan los 26 minutos el isotopo de plutonio se divide en la mitad
 			vueltas++;
 		}
-		while((plutonio*100)>porcentaje);
+		while((plutonio*100)>porcentaje);							  //el bucle continua hasta que el porcentaje que queda del isotopo sea menor que el introducido por el usuario
 		System.out.printf("El tiempo de vida más cercano es %d minutos",(26*vueltas));
 		
 	}
